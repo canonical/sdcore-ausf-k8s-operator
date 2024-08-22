3c0344fd65a3a13b55b7d9311966fa37c0f56ab9
guillaume <guillaume.belanger27@gmail.com>
2024-08-22 17:12:32 -0400

    chore: bump tls lib to v4
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/unit/fixtures.py
+++ tests/unit/fixtures.py
@@ -11,26 +11,16 @@ from charm import AUSFOperatorCharm
 
 class AUSFUnitTestFixtures:
     patcher_check_output = patch("charm.check_output")
-    patcher_generate_csr = patch("charm.generate_csr")
-    patcher_generate_private_key = patch("charm.generate_private_key")
-    patcher_get_assigned_certificates = patch(
-        "charms.tls_certificates_interface.v3.tls_certificates.TLSCertificatesRequiresV3.get_assigned_certificates"
-    )
-    patcher_request_certificate_creation = patch(
-        "charms.tls_certificates_interface.v3.tls_certificates.TLSCertificatesRequiresV3.request_certificate_creation"
+    patcher_get_assigned_certificate = patch(
+        "charms.tls_certificates_interface.v4.tls_certificates.TLSCertificatesRequiresV4.get_assigned_certificate"
     )
     patcher_restart = patch("ops.model.Container.restart")
 
     @pytest.fixture(autouse=True)
     def setup(self, request):
         self.mock_check_output = AUSFUnitTestFixtures.patcher_check_output.start()
-        self.mock_generate_csr = AUSFUnitTestFixtures.patcher_generate_csr.start()
-        self.mock_generate_private_key = AUSFUnitTestFixtures.patcher_generate_private_key.start()
-        self.mock_get_assigned_certificates = (
-            AUSFUnitTestFixtures.patcher_get_assigned_certificates.start()
-        )
-        self.mock_request_certificate_creation = (
-            AUSFUnitTestFixtures.patcher_request_certificate_creation.start()
+        self.mock_get_assigned_certificate = (
+            AUSFUnitTestFixtures.patcher_get_assigned_certificate.start()
         )
         self.mock_restart = AUSFUnitTestFixtures.patcher_restart.start()
         yield

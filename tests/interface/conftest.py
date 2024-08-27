56140d4656a61d7403fca1f5a90e48e68b8fc9c9
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-08-27 09:52:54 +0000

    fix sdcore_config tests

---
+++ tests/interface/conftest.py
@@ -0,0 +1,41 @@
+import tempfile
+
+import pytest
+import scenario
+from interface_tester import InterfaceTester
+from ops.pebble import Layer, ServiceStatus
+
+from charm import AUSFOperatorCharm
+
+
+@pytest.fixture
+def interface_tester(interface_tester: InterfaceTester):
+    with tempfile.TemporaryDirectory() as tempdir:
+        certificates_relation = scenario.Relation(
+            endpoint="certificates",
+            interface="tls-certificates",
+        )
+        certs_mount = scenario.Mount(
+            location="/support/TLS",
+            src=tempdir,
+        )
+        config_mount = scenario.Mount(
+            location="/free5gc/config",
+            src=tempdir,
+        )
+        container = scenario.Container(
+            name="ausf",
+            can_connect=True,
+            mounts={"config": config_mount, "certs": certs_mount},
+            layers={"ausf": Layer({"services": {"ausf": {}}})},
+            service_status={"ausf": ServiceStatus.ACTIVE},
+        )
+        interface_tester.configure(
+            charm_type=AUSFOperatorCharm,
+            state_template=scenario.State(
+                leader=True,
+                relations=[certificates_relation],
+                containers=[container],
+            ),
+        )
+        yield interface_tester
